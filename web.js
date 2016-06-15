e2a8e960a1d1eb8ba56c2b114c1bdd2ca1ae7fd2
Vimal Kaul <kaul.vimal@gmail.com>
2016-06-16 01:58:54 +0300

    as

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(process.env.PORT, "0.0.0.0", function () {
   var host = server.address().address;
   var port = server.address().port;
 
